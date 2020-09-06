119ebc3ef7dfcbd80e38125dcd4c4a98bf94671c
MrSliddes <mrsliddes@outlook.com>
2020-09-06 13:01:21 +0200

    Tymon_Commit_3
    Updated code with comments

--- Kernmodule_Assignment/Assets/Tymon/Tymon_Main.cs
+++ Kernmodule_Assignment/Assets/Tymon/Tymon_Main.cs
@@ -2,19 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// The main class where the monobehaviour is located and the other classes are updated
+/// </summary>
 public class Tymon_Main : MonoBehaviour
 {
+    /// <summary>
+    /// The transform of the pongball (the ball that bounces over the screen)
+    /// </summary>
     public Transform pongball;
+    /// <summary>
+    /// The transform of the player, this one is located on the right side of the screen
+    /// </summary>
     public Transform player;
+    /// <summary>
+    /// The transform of the enemy, this one is located on the left side of the screen
+    /// </summary>
     public Transform enemy;
+
+    /// <summary>
+    /// Reference to the pongball class
+    /// </summary>
     private Tymon_Pongball tymon_pongball;
+    /// <summary>
+    /// Reference to the player class
+    /// </summary>
     private Tymon_Player tymon_player;
+    /// <summary>
+    /// Reference to the enemy class
+    /// </summary>
     private Tymon_Enemy tymon_enemy;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Create a transform array for the ball to check for collisions
         Transform[] arr = { player, enemy };
+        // Set the class references/ create the classes
         tymon_pongball = new Tymon_Pongball(pongball, 5f, arr);
         tymon_player = new Tymon_Player(player, 10f, 8f);
         tymon_enemy = new Tymon_Enemy(enemy, pongball, -7f, 11f);
@@ -23,6 +47,7 @@ public class Tymon_Main : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Update the classes via the Update Methode (Not the monobehaivor Update()!)
         tymon_pongball.Update();
         tymon_player.Update();
         tymon_enemy.Update();
--- Kernmodule_Assignment/Assets/Tymon/Tymon_Pongball.cs
+++ Kernmodule_Assignment/Assets/Tymon/Tymon_Pongball.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// The class of the pongball that bounces in the screen
+/// </summary>
 public class Tymon_Pongball
 {
+    /// <summary>
+    /// The direction of the ball x-axis
+    /// </summary>
     private int dirX = 1;
+    /// <summary>
+    /// The direction of the ball y-axis
+    /// </summary>
     private int dirY = 1;
+    /// <summary>
+    /// The current movement speed of the ball
+    /// </summary>
     private float movementSpeed;
 
+    /// <summary>
+    /// Reference to the pongball transform
+    /// </summary>
     private Transform pongball;
+    /// <summary>
+    /// Reference to the transform of the player and enemy (for collision)
+    /// </summary>
     private Transform[] bars;
 
+    /// <summary>
+    /// Set values when creating this class
+    /// </summary>
+    /// <param name="pongball">The transform of the pongball</param>
+    /// <param name="movementSpeed">The movementspeed of the pongball</param>
+    /// <param name="bars">The bars, meaning the player and enemy "blocks" on either side of the screen</param>
     public Tymon_Pongball(Transform pongball, float movementSpeed, Transform[] bars)
     {
         this.pongball = pongball;
@@ -19,7 +43,7 @@ public class Tymon_Pongball
     }
 
     /// <summary>
-    /// Called to update the ball;
+    /// Called by Tymon_Main to update this class
     /// </summary>
     public void Update()
     {
@@ -27,6 +51,9 @@ public class Tymon_Pongball
         Collision();
     }
 
+    /// <summary>
+    /// Moves the pongball in the right direction
+    /// </summary>
     private void Movement()
     {
         // Move
@@ -36,13 +63,17 @@ public class Tymon_Pongball
         pos.x = Mathf.Clamp01(pos.x);
         pos.y = Mathf.Clamp01(pos.y);
         pongball.position = Camera.main.ViewportToWorldPoint(pos);
-        // Change dir if reached edge
+        // Change direction if it reached the edge of screen
         if(pos.x == 0) dirX = 1; else if(pos.x == 1) dirX = -1;
         if(pos.y == 0) dirY = 1; else if(pos.y == 1) dirY = -1;
     }
 
+    /// <summary>
+    /// Check for collisions with the ball
+    /// </summary>
     private void Collision()
     {
+        // Loop trough all transforms to check collision with
         foreach(Transform bar in bars)
         {
             // Check if ball pos hits barpos ( Bounce off),
@@ -52,7 +83,7 @@ public class Tymon_Pongball
                 // Check y
                 if(pongball.position.y <= bar.position.y + bar.localScale.y && pongball.position.y >= bar.position.y - bar.localScale.y)
                 {
-                    // Bounche off
+                    // Collision, bounche off in right direction
                     if(pongball.position.x < bar.position.x) dirX = -1; else dirX = 1;
                     if(pongball.position.y < bar.position.y) dirY = 1; else dirY = -1;
                 }
